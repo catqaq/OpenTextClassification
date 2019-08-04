a0f816b663cb648121a247ff1862b260cc93895c
catqaq <jjgxw@outlook.com>
2019-08-04 23:01:00 +0800

    add sim_cnn.py

--- main.py
+++ main.py
@@ -6,56 +6,23 @@ Created on Tue Apr  9 10:13:19 2019
 @author: jjg
 """
 
-import pandas as pd
-import numpy as np
 import torch
 import os
+os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
+import numpy as np
+import copy
 from torchtext import data
 from torchtext.vocab import Vectors
-from torch.nn import init
-from tqdm import tqdm
 from nltk.tokenize import word_tokenize
-#from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
-from models import TextCNN, LSTM, GRU, Sim_CNN
+from models import TextCNN, LSTM, GRU, Hybrid_CNN
+from sim_cnn import Sim_CNN
 from settings import Settings
-from train_eval import train, eval
+from train_eval import training, evaluating
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-#load data
-train_set = pd.read_csv(
-    '../yahoo_answers_csv/train.csv',
-    header=None,
-    sep=',',
-    names=['label', 'question_title', 'question_content', 'answer'])
-
-test_set = pd.read_csv(
-    '../yahoo_answers_csv/test.csv',
-    header=None,
-    sep=',',
-    names=['label', 'question_title', 'question_content', 'answer'])
-
-class_vecs = torch.from_numpy(np.load('../yahoo_class_vec_based_on_classname.npy'))
-
-#subset: 1000 samples/ class
-n = 1000
-train_set = pd.concat(
-    [train_set[train_set.label == i][:n] for i in range(1, 11)],
-    ignore_index=True)
-
-
-def preprocess(dataset):
-    dataset.fillna('', inplace=True)
-    dataset['full'] = dataset['question_title'].str.cat(
-        [dataset['question_content'], dataset['answer']], sep=' ')
-    dataset.drop(
-        columns=['question_title', 'question_content', 'answer'], inplace=True)
-    dataset['full'] = dataset['full'].apply(lambda x: word_tokenize(str(x)))
-    return dataset
-
-
-train_set = preprocess(train_set)
-test_set = preprocess(test_set)
+data_path = '/home/jjg/data/text_classification/dbpedia'
+class_vecs = torch.from_numpy(np.load(data_path+'/classname_based_class_vecs.npy'))
+length=100
 
 cache = 'mycache'
 if not os.path.exists(cache):
@@ -67,101 +34,93 @@ vectors = Vectors(
 ############################### construct vocab ##################
 
 #we've finished tokenization above，set tokenize=None, better set batch_first=True
-TEXT = data.Field(sequential=True, lower=True, fix_length=300, batch_first=True)
+TEXT = data.Field(
+    sequential=True,
+    tokenize=word_tokenize,
+    lower=True,
+    fix_length=length,    #according to the dataset
+    batch_first=True)
   
 LABEL = data.Field(sequential=False, use_vocab=False, batch_first=True)
-TEXT.build_vocab(
-    train_set['full'],
-    test_set['full'],
-    vectors=vectors,
-    unk_init=init.xavier_uniform_)
 
+train, test = data.TabularDataset.splits(
+        path=data_path,
+        train='train.csv',
+        test='test.csv',
+        format='csv',
+        fields=[('label', LABEL), ('text', TEXT)],
+        skip_header=True)
+
+#split validation set
+train, dev = train.split(split_ratio=0.7)
+
+#construct the vocab
+#TEXT.build_vocab(train, vectors="glove.840B.300d")
+TEXT.build_vocab(train, vectors=vectors)
+vocab = TEXT.vocab
 del vectors               #del vectors to save space
 
 
-# construct examples and fields
-def get_dataset(csv_data, text_field, label_field):
-
-    fields = [("full", text_field), ("label", label_field)]
-    examples = []
-    for text, label in tqdm(zip(csv_data['full'], csv_data['label'])):
-        examples.append(data.Example.fromlist([text, label], fields))
-    return examples, fields
-
-
-train_examples, train_fields = get_dataset(train_set, TEXT, LABEL)
-test_examples, test_fields = get_dataset(test_set, TEXT, LABEL)
-
-# construct dataset
-train_dataset = data.Dataset(train_examples, train_fields)
-test_dataset = data.Dataset(test_examples, test_fields)
-
-#split validation set
-train_dataset, val_dataset = train_dataset.split(split_ratio=0.7)
-
 #can choose 3 kinds of models
-models = {'CNN': TextCNN, 'LSTM': LSTM, 'GRU': GRU, 'Sim_CNN': Sim_CNN}
+models = {'CNN': TextCNN, 'LSTM': LSTM, 'GRU': GRU, 'Sim_CNN': Sim_CNN, 'Hybrid': Hybrid_CNN}
 
 if __name__ == '__main__':
     #settings
-    classifier='CNN'         #choose CNN/LSTM/GRU
+    #classifier='Sim_CNN'         #choose CNN/LSTM/GRU
     #see settings.py for detail
     label_vecs =class_vecs.unsqueeze(1).unsqueeze(2)
     args = Settings(
-        TEXT.vocab.vectors,
+        vocab.vectors,            #pre-trained word embeddings
         label_vecs,
-        Dim=300,
-        Cout=64,
-        kernel_size=[3, 4, 5],
+        L=length,
+        Dim=300,                       #embedding dimension
+        num_class=14,
+        Cout=256,                       #kernel numbers
+        kernel_size=[3, 4, 5],         #different kernel size
         dropout=0.5,
-        batch_size=64,
+        batch_size=256,
         num_epochs=10,
-        lr=0.001,
+        lr=0.0001,
         weight_decay=0,
-        static=True,
+        static=4,                   #update the embeddings or not
         batch_normalization=False,
         hidden_size=100,
         rnn_layers=2,
         bidirectional=False)
     
     #construct dataset iterator
-    train_iter, val_iter = data.BucketIterator.splits(
-        datasets=(train_dataset, val_dataset),
-        batch_sizes=(args.batch_size, args.batch_size),
-        sort_key=lambda x: len(x.full),
-        shuffle=True,
-        sort_within_batch=False,
-        repeat=False)
-    test_iter = data.Iterator(
-        test_dataset,
-        batch_size=args.batch_size,
-        sort=False,
-        sort_within_batch=False,
-        repeat=False)
-
-    if args.static:
-        print('static %s(without updating embeddings):' % classifier)
-    else:
-        print('non-static %s(update embeddings):' % classifier)
+    train_iter, dev_iter, test_iter = data.BucketIterator.splits(
+           (train, dev, test),
+           sort_key=lambda x: len(x.text), 
+           batch_sizes=(args.batch_size, args.batch_size, args.batch_size),
+           )
     
-    test_acc = []
-    t = 5  #repeat times
-    max_acc = 0
-    for i in range(t):
-        model = models[classifier](args)
-        train(train_iter, val_iter, model, args, device)
-        tmp = eval(test_iter, model, device)
-        test_acc.append(tmp)
-        if tmp > max_acc:
-            max_acc = tmp
-            torch.save(model.state_dict(), './result/'+classifier+'.pt')
-    print('Repeat %s times: %s' % (t, test_acc))
-    print('average test_acc: %.1f%%' % (100*sum(test_acc)/t))
-    print('max acc: %.1f%%' % (100*max_acc))
-    
-    #load the trained model
-    model = models[classifier](args)
-    model.load_state_dict(torch.load('./result/'+classifier+'.pt'))
-    model.eval()
-    print(eval(test_iter, model, device))
+    res = []
+    for classifier in ['CNN']:
+        
+        if args.static:
+            print('static %s(without updating embeddings):' % classifier)
+        else:
+            print('non-static %s(update embeddings):' % classifier)
+        
+        dev_acc = []
+        t = 1  #repeat times
+        best_acc = 0
+        for i in range(t):
+            model = models[classifier](args)
+            training(train_iter, dev_iter, model, args, device)
+            tmp = evaluating(dev_iter, model, device)
+            dev_acc.append(tmp)
+            if tmp > best_acc:
+                best_acc = tmp
+                best_model_wts = copy.deepcopy(model.state_dict())
+        print('Repeat %s times: %s' % (t, dev_acc))
+        print('average dev_acc: %.1f%%' % (100*sum(dev_acc)/t))
+        print('max dev_acc: %.1f%%' % (100*best_acc))
+        model.load_state_dict(best_model_wts)
+        #finally evaluate the test set
+        test_acc = evaluating(test_iter, model, device)
+        print('test acc: %.1f%%' % (test_acc*100))
+        res.append(test_acc)
+    print(res)
     
--- models.py
+++ models.py
@@ -9,6 +9,7 @@ Created on Tue Apr 16 22:27:48 2019
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import math
 
 
 class TextCNN(nn.Module):
@@ -27,7 +28,7 @@ class TextCNN(nn.Module):
         
         self.embed = nn.Embedding(V, D)
         self.embed.weight.data.copy_(weight_matrix)
-        if static:
+        if static in [1, 2]:
             self.embed.weight.requires_grad=False
         else:
             self.embed.weight.requires_grad=True
@@ -39,14 +40,14 @@ class TextCNN(nn.Module):
         self.fc1 = nn.Linear(len(Ks)*Co, C)
 
     def forward(self, x):
-        x = self.embed(x)  # (N, W, D)
+        x = self.embed(x)  # (N, L, D)
         
-        x = x.unsqueeze(1)  # (N, Ci, W, D), insert a dimention of size one(in_channels Ci)
+        x = x.unsqueeze(1)  # (N, Ci, L, D), insert a dimention of size one(in_channels Ci)
         
         if self.use_bn:
             x=self.bn2d(x)
         #ModuleList can act as iterable, or be indexed using ints
-        x = [F.relu(conv(x)).squeeze(3) for conv in self.convs1] #[(N, Co, W), ...]*len(Ks)
+        x = [F.relu(conv(x)).squeeze(3) for conv in self.convs1] #[(N, Co, L), ...]*len(Ks)
 
         x = [F.max_pool1d(i, i.size(2)).squeeze(2) for i in x]  # [(N, Co), ...]*len(Ks)
         
@@ -58,13 +59,14 @@ class TextCNN(nn.Module):
         return x
 
 
-class Sim_CNN(nn.Module):
+class Hybrid_CNN(nn.Module):
     
     def __init__(self, args):
-        super(Sim_CNN, self).__init__()
+        super(Hybrid_CNN, self).__init__()
         V = args.num_embeddings
+        self.L = args.L
         D = args.D
-        C = args.C
+        self.C = args.C
         Ci = args.Ci
         Co = args.Co
         Ks = args.Ks
@@ -74,53 +76,110 @@ class Sim_CNN(nn.Module):
         
         self.embed = nn.Embedding(V, D)
         self.embed.weight.data.copy_(weight_matrix)
-        if static:
-            self.embed.weight.requires_grad=False
-        else:
-            self.embed.weight.requires_grad=True
         
         self.bn2d = nn.BatchNorm2d(1,momentum=0.1)
         #can keep the size by set padding=(kernel_size-1)//2, if stride=1
         self.convs1 = nn.ModuleList([nn.Conv2d(Ci, Co, (K, D), padding=((K-1)//2,0)) for K in Ks])
-        self.conv_sim = nn.Conv2d(Ci, C, (1, D)) #10 label vec kernel with size(1,D)
+        self.conv_sim = nn.Conv2d(Ci, self.C, (1, D)) #10 label vec kernel with size(1,D)
         self.conv_sim.weight = torch.nn.Parameter(args.label_vecs)
-        #self.conv_sim.weight.requires_grad=False
-        self.conv_doc = nn.Conv2d(C, C, (300, 1), stride=(300,1))
+        if static == 1:
+            self.embed.weight.requires_grad=False
+            self.conv_sim.weight.requires_grad=False
+        elif static == 2:
+            self.embed.weight.requires_grad=False
+        elif static == 3:
+            self.conv_sim.weight.requires_grad=False
+        self.conv_doc = nn.Conv2d(self.C, self.C, (300, 1), stride=(300,1))
         
         self.dropout = nn.Dropout(args.drop)
-        self.fc1 = nn.Linear(len(Ks)*Co, C)
-        self.fc2 = nn.Linear(20, C)
-        self.fc3 = nn.Linear(C, C)
-        self.fc4 = nn.Linear(len(Ks)*Co+C, C)
+        self.fc1 = nn.Linear(len(Ks)*Co, self.C)
+        self.fc2 = nn.Linear(20, self.C)
+        self.fc3 = nn.Linear(self.L, 1)
+        self.fc4 = nn.Linear(len(Ks)*Co+self.C, self.C)
+        self.fc5 = nn.Linear(len(Ks)*Co+self.L*self.C, self.C)
+        self.fc6 = nn.Linear(self.L*self.C, self.C)
+        self.fc7 = nn.Linear(self.C, self.C)
 
     def forward(self, x):
-        x = self.embed(x)  # (N, W, D)
-        
-        x = x.unsqueeze(1)  # (N, Ci, W, D), insert a dimention of size one(in_channels Ci)
+        x = self.embed(x)  # (N, L, D)
+            
+        x = x.unsqueeze(1)  # (N, Ci, L, D), insert a dimention of size one(in_channels Ci)
         
         if self.use_bn:
             x=self.bn2d(x)
         
-        #random initialization kernel + label vector initialization kernel
-        #random kernel
-        r1 = [F.relu(conv(x)).squeeze(3) for conv in self.convs1] #[(N, Co, W), ...]*len(Ks)
-
-        r1 = [F.max_pool1d(i, i.size(2)).squeeze(2) for i in r1]  # [(N, Co), ...]*len(Ks)
-        
-        r1 = torch.cat(r1, 1)   #concatenate different feature from different kernel sizes
-
-        r1 = self.dropout(r1)  # (N, len(Ks)*Co)
-        
+        r1 = [F.relu(conv(x)).squeeze(3) for conv in self.convs1] #[(N, Co, L), ...]*len(Ks)
+        r1 = [F.max_pool1d(i, i.size(2)).squeeze(2) for i in r1]  # [(N, Co), ...]*len(Ks) 
+        r1 = torch.cat(r1, 1)   #(N, len(Ks)*Cout)
+        r1 = self.dropout(r1)  
         r1 = self.fc1(r1)  # (N, C)
         
-        #label vector kernel
-        r2 = self.conv_sim(x) #(N, 10, W)
-        r2 = r2.squeeze(3)
-        r2 = F.avg_pool1d(r2, r2.size(2)).squeeze(2) #(N, 10)
-        #r2 = F.max_pool1d(r2, r2.size(2)).squeeze(2)     
-        return r1+r2
+        x = self.conv_sim(x).squeeze() #(N, 10, L)
+        x = [self.fc3(self.dropout(i)) for i in torch.chunk(x, self.C, 1)]
+        x = torch.cat(x, 1).squeeze()
         
+        return x+r1
+        
+
+class Sim_CNN(nn.Module):
+    """
+    use Sim_CNN to learn label presentation and compute similarities to help classification
+    """
     
+    def __init__(self, args):
+        super(Sim_CNN, self).__init__()
+        self.V = args.num_embeddings
+        self.L = args.L
+        self.D = args.D
+        self.C = args.C
+        Ci = args.Ci
+        weight_matrix = args.weight
+        static = args.static
+        self.use_bn = args.use_bn
+        
+        self.embed = nn.Embedding(self.V, self.D)
+        self.embed.weight.data.copy_(weight_matrix)
+        self.pe1 = PositionalEncoding(self.D, args.drop)
+        self.pe2 = PE(self.L, max_len=self.V)
+        
+        self.bn2d = nn.BatchNorm2d(1,momentum=0.1)
+        self.conv_sim = nn.Conv2d(Ci, self.C, (1, self.D)) #10 label vec kernel with size(1,D)
+        self.conv_sim.weight = torch.nn.Parameter(args.label_vecs)
+        self.construct = nn.Conv2d(Ci, self.C, (self.L, 1)) #learn doc constructure
+        self.multi_cons = nn.Conv2d(Ci, self.C*10, (self.L, 1))
+        if static == 1:
+            self.embed.weight.requires_grad=False
+            self.conv_sim.weight.requires_grad=False
+        elif static == 2:
+            self.embed.weight.requires_grad=False
+        elif static == 3:
+            self.conv_sim.weight.requires_grad=False
+        
+        self.dropout = nn.Dropout(args.drop)
+        self.fc1 = nn.Linear(self.L*self.C, self.C)
+        self.fc2 = nn.ModuleList([nn.Linear(self.L, 1) for i in range(self.C)])
+        self.fc3 = nn.Linear(self.L, 1)
+        self.fc4 = nn.Linear(self.L, self.C)
+        self.fc5 = nn.Linear(self.C*10, self.C)
+
+    def forward(self, x):
+        pe = self.pe2(x)   # (N, L)
+        pe = pe.unsqueeze(1).unsqueeze(3)
+        pe = self.multi_cons(pe).squeeze()
+        x = self.embed(x)  # (N, L, D)
+        
+        #x = self.pe1(x)     #word embedding + position embedding
+            
+        x = x.unsqueeze(1)  # (N, Ci, L, D), insert a dimention of size one(in_channels Ci)
+        
+        if self.use_bn:
+            x=self.bn2d(x)
+        
+        x = self.conv_sim(x).squeeze() #(N, C, L)
+        x = [self.fc3(self.dropout(i)) for i in torch.chunk(x, self.C, 1)]
+        x = torch.cat(x, 1).squeeze()
+        x = x + self.fc5(self.dropout(pe))
+        return x
 
 class LSTM(nn.Module):
     def __init__(self, args):
@@ -227,4 +286,36 @@ class GRU(nn.Module):
         # choose r_out at the last time step
         out = self.fc(r_out[:, -1, :]) #(batch_size,num_classes)
         return out
+
+class PositionalEncoding(nn.Module):
+    "Implement the PE function."
+    def __init__(self, d_model, dropout, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+        # Compute the positional encodings once in log space.
+        pe = torch.zeros(max_len, d_model) #max_len: max doc length
+        position = torch.arange(0., max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0., d_model, 2) *-(math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+        
+    def forward(self, x):
+        x = x + (self.pe[:, :x.size(1)]).clone().detach()#为啥+x? 这里的x是词嵌入后的结果，因此这里其实是词嵌入+位置嵌入
+        return self.dropout(x)
+
+
+class PE(nn.Module):
+    "a simple PE version without considering dimention"
+    def __init__(self, L, max_len=5000):
+        super(PE, self).__init__()
+        self.L = L
+        self.max_len = max_len
+        pass #any better way?
+        
     
+    def forward(self, x):
+        x = torch.sin(x.float()/self.max_len) #div_term:1, L^0.5, L, len(vocab)?
+        return x
+        
